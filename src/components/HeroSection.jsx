ffe50da71799726a19f5f7882399afff77aff254
tabrez <04tabrez@gmail.com>
2025-08-08 01:29:18 +0530

    fix:overlapping issue of navbar is fixed

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/autoplay';
 
 function HeroSection() {
   return (
-    <div>
-      
+    <div >
+         <Swiper
+         modules={[Autoplay]}
+         autoplay={{ delay: 3000, disableOnInteraction: false }}
+         loop={true}
+         className="h-80 w-full "
+          >
+       <SwiperSlide className="bg-[url('src/assets/SofaPhoto-1.jpg')] bg-cover bg-center text-black flex items-center justify-center text-4xl font-bold">
+      "Bring Your Sofa Back to Life!"
+       </SwiperSlide>
+       <SwiperSlide className="bg-[url('src/assets/SofaPhoto-2.webp')] bg-cover bg-center text-black flex items-center justify-center text-4xl font-bold">
+       "Expert Repair with a Personal Touch"
+       </SwiperSlide>
+       <SwiperSlide className="bg-[url('src/assets/SofaPhoto-3.webp')] bg-cover bg-center text-black flex items-center justify-center text-4xl font-bold">
+      "Old Sofa, New Life"
+      </SwiperSlide>
+      </Swiper>
     </div>
   )
 }
