5578c34f26ec336624dee51c6e3a50ae69957067
tabrez <04tabrez@gmail.com>
2025-08-08 13:25:01 +0530

    style : navgiation btn and pagination

--- src/App.jsx
+++ src/App.jsx
@@ -15,16 +15,16 @@ function App() {
         </main>
         
 
-      <div className="fixed bottom-5 left-5 z-50">
+    <div className="fixed bottom-5 left-5 z-50">
   <div className="relative w-14 h-14">
     {/* Ripple Layer 1 */}
-    <span className="absolute left-1.5 top-1.5 w-11 h-11 rounded-full bg-[#3D74B6] opacity-80 smooth-ripple duration-[2000ms] ease-in-out"></span>
+    <span className="absolute left-1.5 top-1.5 w-11 h-11 rounded-full bg-[#3D74B6] opacity-80 smooth-ripple duration-[1000ms] ease-in-out"></span>
 
     {/* Ripple Layer 2 */}
-    <span className="absolute left-1.5 top-1.5 w-11 h-11 rounded-full bg-[#3D74B6] opacity-60 smooth-ripple duration-[3000ms] ease-in-out delay-[500ms]"></span>
+    <span className="absolute left-1.5 top-1.5 w-11 h-11 rounded-full bg-[#3D74B6] opacity-60 smooth-ripple duration-[2000ms] ease-in-out delay-[500ms]"></span>
 
     {/* Ripple Layer 3 */}
-    <span className="absolute left-1.5 top-1.5 w-11 h-11 rounded-full bg-[#3D74B6] opacity-40 smooth-ripple duration-[4000ms] ease-in-out delay-[500ms]"></span>
+    <span className="absolute left-1.5 top-1.5 w-11 h-11 rounded-full bg-[#3D74B6] opacity-40 smooth-ripple duration-[3000ms] ease-in-out delay-[500ms]"></span>
 
     {/* Phone button */}
     <a href="tel:+917846940025">
@@ -38,7 +38,7 @@ function App() {
           {/* whatsapp button  */}
          <a href="https://wa.me/917846940025?text=Hi%2C%20I%20need%20a%20sofa%20repair%20service" 
           target="_blank" rel="noopener noreferrer">
-         <button className="w-14 h-14 rounded-full bg-[#25D366] bottom-5 right-5 fixed">
+         <button className="w-14 h-14 rounded-full bg-[#25D366] bottom-5 right-5 z-50 fixed">
           <i className="text-4xl  px-1  text-white  ri-whatsapp-line"></i>
          </button>
          </a>
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,30 +1,47 @@
-import React from 'react'
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay } from 'swiper/modules';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/autoplay';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
 
 function HeroSection() {
   return (
-    <div >
-         <Swiper
-         modules={[Autoplay]}
-         autoplay={{ delay: 3000, disableOnInteraction: false }}
-         loop={true}
-         className="h-80 w-full "
-          >
-       <SwiperSlide className="bg-[url('src/assets/SofaPhoto-1.jpg')] bg-cover bg-center text-black flex items-center justify-center text-4xl font-bold">
-      "Bring Your Sofa Back to Life!"
-       </SwiperSlide>
-       <SwiperSlide className="bg-[url('src/assets/SofaPhoto-2.webp')] bg-cover bg-center text-black flex items-center justify-center text-4xl font-bold">
-       "Expert Repair with a Personal Touch"
-       </SwiperSlide>
-       <SwiperSlide className="bg-[url('src/assets/SofaPhoto-3.webp')] bg-cover bg-center text-black flex items-center justify-center text-4xl font-bold">
-      "Old Sofa, New Life"
-      </SwiperSlide>
+    <div className="relative text-black">
+
+      {/* Custom Navigation Buttons */}
+      <div className="swiper-button-prev absolute left-4 top-1/2 z-10 transform -translate-y-1/2 cursor-pointer !text-[#F5DEB3]">
+        <ChevronLeft size={32} />
+      </div>
+      <div className="swiper-button-next absolute right-4 top-1/2 z-10 transform -translate-y-1/2 cursor-pointer !text-[#F5DEB3]">
+        <ChevronRight size={32} />
+      </div>
+
+      <Swiper
+        modules={[Navigation, Autoplay, Pagination]}
+        navigation={{
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        }}
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 3000, disableOnInteraction: false }}
+        loop={true}
+        className="h-[35rem] w-full"
+      >
+        <SwiperSlide className="bg-[url('src/assets/SofaPhoto-1.jpg')] bg-cover bg-center flex items-center justify-center text-4xl font-bold">
+          <h1 className="text-center mt-18 text-white">"Bring Your Sofa Back to Life!"</h1>
+        </SwiperSlide>
+        <SwiperSlide className="bg-[url('src/assets/SofaPhoto-2.webp')] bg-cover bg-center flex items-center justify-center text-4xl font-bold">
+          <h1 className="text-center mt-18 text-white">"Expert Repair with a Personal Touch"</h1>
+        </SwiperSlide>
+        <SwiperSlide className="bg-[url('src/assets/SofaPhoto-3.webp')] bg-cover bg-center flex items-center justify-center text-4xl font-bold">
+          <h1 className="text-center mt-18 text-white">"Old Sofa, New Life"</h1>
+        </SwiperSlide>
       </Swiper>
     </div>
-  )
+  );
 }
 
-export default HeroSection
+export default HeroSection;
