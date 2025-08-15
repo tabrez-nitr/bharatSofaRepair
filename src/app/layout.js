14f651e306e3712e7c189a4c5e19e41ec3e6412d
tabrez <04tabrez@gmail.com>
2025-08-15 20:47:38 +0530

    speed test using vercel

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,6 +25,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
        >
         {children}
+         <SpeedInsights />
       </body>
     </html>
   );
